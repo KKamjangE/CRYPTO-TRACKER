260bf8c7c2a625c8e38d212077cf889d373b7f3d
KKamjangE <ajm980718@gmail.com>
2023-02-08 20:14:24 +0900

    feat: Price Chart 3.0

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -60,6 +60,20 @@ function Chart({ coinId }: ChartProps) {
               axisBorder: { show: false },
               axisTicks: { show: false },
               labels: { show: false },
+              categories: data?.map((price) =>
+                new Date(price.time_close * 1000).toUTCString()
+              ),
+              type: "datetime",
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value.toFixed(3)}`,
+              },
             },
           }}
         />

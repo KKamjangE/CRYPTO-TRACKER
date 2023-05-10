6e6bb2d0b506ce5477a97f4ff0ec82c84215757e
KKamjangE <ajm980718@gmail.com>
2023-05-10 16:49:27 +0900

    Refactor: 스타일 코드 위치 수정

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -6,6 +6,55 @@ import { useSetRecoilState } from "recoil";
 import { fetchCoins } from "@/api";
 import { isDarkAtom } from "@/atoms";
 
+interface ICoin {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const setDarkAtom = useSetRecoilState(isDarkAtom);
+  const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
+  const { isLoading, data } = useQuery<ICoin[]>(["allCoins"], fetchCoins);
+  return (
+    <Container>
+      <Helmet>
+        <title>Coin Tracker</title>
+      </Helmet>
+      <Header>
+        <Title>Coin Tracker</Title>
+        <button onClick={toggleDarkAtom}>Toggle Mode</button>
+      </Header>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {data?.slice(0, 100).map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                }}
+                state={{ name: coin.name }}
+              >
+                <Img
+                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                  alt="img"
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
+}
+
 const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
@@ -49,54 +98,5 @@ const Img = styled.img`
   height: 35px;
   margin-right: 10px;
 `;
-
-interface ICoin {
-  id: string;
-  name: string;
-  symbol: string;
-  rank: number;
-  is_new: boolean;
-  is_active: boolean;
-  type: string;
-}
-
-function Coins() {
-  const setDarkAtom = useSetRecoilState(isDarkAtom);
-  const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
-  const { isLoading, data } = useQuery<ICoin[]>(["allCoins"], fetchCoins);
-  return (
-    <Container>
-      <Helmet>
-        <title>코인</title>
-      </Helmet>
-      <Header>
-        <Title>코인</Title>
-        <button onClick={toggleDarkAtom}>Toggle Mode</button>
-      </Header>
-      {isLoading ? (
-        <Loader>Loading...</Loader>
-      ) : (
-        <CoinsList>
-          {data?.slice(0, 100).map((coin) => (
-            <Coin key={coin.id}>
-              <Link
-                to={{
-                  pathname: `/${coin.id}`,
-                }}
-                state={{ name: coin.name }}
-              >
-                <Img
-                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
-                  alt="img"
-                />
-                {coin.name} &rarr;
-              </Link>
-            </Coin>
-          ))}
-        </CoinsList>
-      )}
-    </Container>
-  );
-}
 
 export default Coins;
